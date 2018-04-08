7f7f52418af050513febb64eaa81f3056bda5ad9
Ryan <cybervoid41@gmail.com>
2018-04-08 09:04:40 -0400

    pointers stuff

--- GoUdemy/LanguageBasics/basics/pointers.go
+++ GoUdemy/LanguageBasics/basics/pointers.go
@@ -8,6 +8,6 @@ func PointerExample(){
   fmt.Println(&a)
   //b is a pointer, b points to a's memory address
   var b *int = &a
-  fmt.Println(b)
-
+  fmt.Println(b) //print the memory address
+  fmt.Println(*b) //print the value at the memory addresses
 }
