d8dccb655c51103d87a5f2317e6b3b401cfd140e
cybervoid <4731255+cybervoid@users.noreply.github.com>
2018-05-04 07:44:20 -0400

    added continue location

--- GoShardingPOC/main/beacon/beacon_chain_node.go
+++ GoShardingPOC/main/beacon/beacon_chain_node.go
@@ -3,3 +3,16 @@ package beacon
 //import (
 //  "../helpers"
 //)
+
+const NOTARIES int = 40
+const BASE_TS_DIFF int = 1
+const SKIP_TS_DIFF int = 6
+const SAMPLE int = 9
+const MIN_SAMPLE int = 5
+const POWDIFF int = 30 * NOTARIES
+const SHARDS int = 4
+
+
+func checkPow(work string, nonce int) {
+  //CONTINUE HERE:
+}
