8200104dab2de457b20433882598d1599220f5d7
cybervoid <4731255+cybervoid@users.noreply.github.com>
2018-06-06 17:29:49 -0400

    hackerrank, more code

--- hackerrank/warmup/warmup.go
+++ hackerrank/warmup/warmup.go
@@ -2,6 +2,38 @@ package main
 
 //Complete the function solveMeFirst to compute the sum of two integers.
 
-int solveMeFirst(a uint32, b uint32) uint32 {
+import "fmt"
+func solveMeFirst(a uint32, b uint32) uint32 {
     return a + b
+}
+
+func simpleArraySum(ar []int32) int32 {
+     var x int32 = 0
+     for i := 0; i < len(ar); i++ {
+            x = ar[i] + x
+         }
+         return x
+}
+// Complete the solve function below.
+func solve(a []int32, b []int32) []int32 {
+    var result []int32 = make([]int32, 2)
+    for i := 0; i < len(a); i++ {
+        if a[i] > b[i] {
+            result[0] = 1
+        } else if a[i] == b[i] {
+            continue
+        } else {
+            result[1] = 1
+        }
+    }
+    return result
+}
+func main() {
+
+     morenums := []int32{1, 2, 3, 4, 10, 11}
+
+     fmt.Println(simpleArraySum(morenums))
+     numa := []int32{5, 6, 7}
+     numb := []int32 {3, 6, 10}
+     solve(numa, numb)
 }
