48a108ed025d032ee9a4b8626e032a2987264be0
cybervoid <4731255+cybervoid@users.noreply.github.com>
2018-06-08 06:35:14 -0400

    hacker rank, 100 hours of go code

--- hackerrank/warmup/warmup.go
+++ hackerrank/warmup/warmup.go
@@ -53,7 +53,67 @@ func runDiagnalDifference() {
 	//fmt.Printf("%t", multi)
 	fmt.Println(diagonalDifference(multi))
 }
-func main() {
 
-	runDiagnalDifference()
+// Complete the plusMinus function below.
+func plusMinus(arr []int32) {
+	var zero = 0
+	var positive = 0
+	var negative = 0
+	var total = 0
+	for i := 0; i < len(arr); i++ {
+		if arr[i] < 0 {
+			negative++
+		}
+		if arr[i] == 0 {
+			zero++
+		}
+		if arr[i] > 0 {
+			positive++
+		}
+		total = i + 1
+	}
+	var percentPositive float32 = 0
+	if positive != 0 {
+		percentPositive = float32(positive) / float32(total)
+	}
+	var percentZero float32 = 0
+	if zero != 0 {
+		percentZero = float32(zero) / float32(total)
+	}
+	var percentNegative float32 = 0
+	if negative != 0 {
+		percentNegative = float32(negative) / float32(total)
+	}
+	fmt.Println(percentPositive)
+	fmt.Println(percentZero)
+	fmt.Println(percentNegative)
+}
+func runPlusMinus() {
+	var arr []int32 = []int32{-4, 3, -9, 0, 4, 1}
+	plusMinus(arr)
+}
+
+func staircase(n int32) {
+	var i int32 = 0
+	for i = 0; i < n; i++ {
+		var output string = ""
+		var j int32 = 0
+		for j = 0; j < n; j++ {
+			if i < j {
+				output = " " + output
+			} else {
+				output = "#" + output
+			}
+		}
+		fmt.Println(output)
+	}
+}
+
+func runStaircase() {
+	staircase(4)
+}
+func main() {
+	runStaircase()
+	//runPlusMinus()
+	//runDiagnalDifference()
 }
