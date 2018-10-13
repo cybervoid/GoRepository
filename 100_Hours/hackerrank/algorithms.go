8102a0d677c86fd742b239ae892d97d5cf15205d
cybervoid <4731255+cybervoid@users.noreply.github.com>
2018-10-13 08:47:21 -0700

    added birthday candles sample problem solution.

--- 100_Hours/hackerrank/algorithms.go
+++ 100_Hours/hackerrank/algorithms.go
@@ -30,18 +30,44 @@ func kangaroo(x1 int32, v1 int32, x2 int32, v2 int32) string {
 }
 func getTotalX(a []int32, b []int32) int32 {
     var total int32 = 0
-    
-
+	return total
 }
+
+// Complete the birthdayCakeCandles function below.
+func birthdayCakeCandles(ar []int32) int32 {
+	var highest int32 = 0
+	var count int32 = 0
+
+	for i := 0; i < len(ar); i++ {
+		if highest < ar[i] {
+			highest = ar[i]
+			count = 0
+		}
+		if highest == ar[i] {
+			count++
+		}
+	}
+	return count
+}
+
+// Complete the birthdayCakeCandles function below.
+func runBirthdayCakeCandles() {
+	var a []int32 = []int32{ 3, 2, 1, 3 }
+	result := birthdayCakeCandles(a);
+	fmt.Println(result)
+}
+
 func runGetTotalX() {
     var a []int32 = []int32 { 2, 4 }
     var b []int32 = []int32 { 16, 32, 96 }
     total := getTotalX(a, b)
+	fmt.Println(total)
 }
 
 func runKangaroo() {
     fmt.Println(kangaroo(0, 2, 5, 3))
 }
 func main() {
-    runKangaroo()
+	runBirthdayCakeCandles()
+    //runKangaroo()
 }
